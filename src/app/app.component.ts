9f540442b6dd1fec36ce6bb67510715a10e71777
Joel Tanzi <jtanziredux@gmail.com>
2017-03-31 18:03:46 -0500

    Updated styling for menu and added thumbnail images.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <nav fxflex fxlayout="row" fxLayoutGap=30px fxLayoutWrap="nowrap" fxLayoutAlign="center center">
-      <a routerLink="/about" routerLinkActive="active">About Us</a>
-      <a routerLink="/coffees" routerLinkActive="active">Our Coffees</a>
+      <a routerLink="/about" routerLinkActive="active">About</a>
+      <a routerLink="/coffees" routerLinkActive="active">Coffees</a>
       <a routerLink="/menu" routerLinkActive="active">Menu</a>
     </nav>
     <router-outlet></router-outlet>
